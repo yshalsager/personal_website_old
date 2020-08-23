5db0f007c654d77108702275054f69be27c253f7
yshalsager <ysh-alsager@hotmail.com>
2020-08-23 17:55:36 +0200

    Templates: Inital base, index, about, and error templates
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- personal_website/main/views.py
+++ personal_website/main/views.py
@@ -1,6 +1,18 @@
+from flask import render_template, url_for
 from . import main
 
 
 @main.route('/')
 def index():
-    return "Hello World"
+    return render_template('index.html',
+                           heading="Clean Blog",
+                           subheading="A Blog Theme by Start Bootstrap",
+                           image=url_for('static', filename='img/home-bg.jpg'))
+
+
+@main.route('/about')
+def about():
+    return render_template('about.html',
+                           heading="About Me",
+                           subheading="This is what I do",
+                           image=url_for('static', filename='img/home-bg.jpg'))
