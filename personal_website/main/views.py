053cf8830cfadce0ec4687c37b9382be4d98fb93
yshalsager <ysh-alsager@hotmail.com>
2020-08-24 11:06:10 +0200

    Initial RTL version
    * Add logo to navigation bar and hide title on small screens
    
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- personal_website/main/views.py
+++ personal_website/main/views.py
@@ -1,23 +1,34 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect
 from . import main
 
 
 @main.route('/')
-def index():
-    return render_template('index.html',
-                           image=url_for('static', filename='img/home-bg.jpg'), safe=True)
+def index_redirect():
+    return redirect('/en/')
 
 
-@main.route('/about')
+@main.route('/en/')
+def index_en():
+    return render_template('index.html', lang="en")
+
+
+@main.route('/ar/')
+def index_ar():
+    return render_template('index_ar.html', lang="ar")
+
+
+@main.route('/en/about')
 def about():
     return render_template('about.html',
                            heading="About Me",
                            subheading="This is what I do",
+                           lang="en",
                            image=url_for('static', filename='img/home-bg.jpg'))
 
 
-@main.route('/blog')
+@main.route('/en/blog')
 def blog():
     return render_template('blog.html',
                            heading="Blog",
+                           lang="en",
                            image=url_for('static', filename='img/home-bg.jpg'))
