d36e66e320599460017d3012a09af750fccc8e41
yshalsager <ysh-alsager@hotmail.com>
2020-08-26 22:10:15 +0200

    Add Blog RSS Feed
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- personal_website/main/posts.py
+++ personal_website/main/posts.py
@@ -1,10 +1,13 @@
-from flask import render_template
+from datetime import datetime, timezone
+from functools import partial
+from pathlib import Path
+
+from feedgen.feed import FeedGenerator
+from flask import render_template, make_response, request
 from flask_babel import force_locale
 
-from .utils.posts import Post
-
-from pathlib import Path
-from functools import partial
+from .utils.posts import Post, get_blog_posts
+from ..main import main
 
 
 def render(lang, post: Post):
@@ -27,3 +30,25 @@ def add_posts_routes(app):
                              file.stem,
                              partial(render_func, lang, Post(file))
                              )
+
+
+@main.route('/<lang>/rss')
+def rss(lang):
+    posts = get_blog_posts(lang)
+    fg = FeedGenerator()
+    fg.title('مدونة يوسف شعبان' if lang == 'ar' else 'yshalsager Blog')
+    fg.description('آخر التدوينات على مدونتي' if lang == 'ar' else 'Latest Feeds from my blog')
+    fg.link(href=request.host_url, rel='alternate')
+    fg.language(lang)
+
+    for post in posts:
+        fe = fg.add_entry()
+        fe.title(post.title)
+        fe.link(href=f"{request.host_url}{lang}/{post.file.stem}")
+        fe.description(post.html)
+        fe.pubDate(datetime.combine(datetime.strptime(post.date, "%d-%m-%Y"), datetime.min.time(), tzinfo=timezone.utc))
+
+    response = make_response(fg.rss_str(pretty=True))
+    response.headers.set('Content-Type', 'application/rss+xml')
+
+    return response
