47553468154c71dcb8ce0fe3b82df103ee79d089
yshalsager <ysh-alsager@hotmail.com>
2020-08-26 20:45:12 +0200

    Add sitemap generation using Flask-Sitemap
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Config:
     LANGUAGES = ['en', 'ar']
     POSTS_DIR = 'posts'
+    SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS = True
--- personal_website/__init__.py
+++ personal_website/__init__.py
@@ -1,16 +1,19 @@
 from flask import Flask
 from flask_babel import Babel
+from flask_sitemap import Sitemap
 
 from config import Config
 from .main.posts import add_posts_routes
 
 babel = Babel()
+sitemap = Sitemap()
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     babel.init_app(app)
+    sitemap.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
