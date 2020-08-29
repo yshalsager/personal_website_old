6647aebcb29a0631ee741c27b8ccf82815372d97
yshalsager <ysh-alsager@hotmail.com>
2020-08-29 23:15:54 +0200

    Add contact page
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- personal_website/main/ar/views.py
+++ personal_website/main/ar/views.py
@@ -8,33 +8,35 @@ from .. import main
 @main.route('/ar/')
 def index_ar():
     with force_locale('ar'):
-        return render_template('index.html', lang="ar")
+        return render_template('index.html', lang="ar", image=url_for('static', filename='img/home-bg.jpg'))
 
 
 @main.route('/ar/about')
 def about_ar():
     with force_locale('ar'):
-        return render_template('about.html',
-                               lang="ar",
-                               heading=_("About Me"),
-                               image=url_for('static', filename='img/home-bg.jpg'))
+        return render_template('about.html', lang="ar",
+                               heading=_("About Me"), image=url_for('static', filename='img/home-bg.jpg'))
 
 
 @main.route('/ar/blog')
 def blog_ar():
     posts = get_blog_posts('ar')
     with force_locale('ar'):
-        return render_template('blog.html',
-                               heading=_("Blog"),
+        return render_template('blog.html', heading=_("Blog"),
                                image=url_for('static', filename='img/home-bg.jpg'),
-                               lang='ar',
-                               posts=posts)
+                               lang='ar', posts=posts)
 
 
 @main.route('/ar/projects')
 def projects_ar():
     with force_locale('ar'):
-        return render_template('projects.html',
-                               heading=_("My Works and Projects"),
-                               lang="ar",
-                               image=url_for('static', filename='img/home-bg.jpg'))
+        return render_template('projects.html', heading=_("My Works and Projects"),
+                               lang="ar", image=url_for('static', filename='img/home-bg.jpg'))
+
+
+@main.route('/ar/contact')
+def contact_ar():
+    with force_locale('ar'):
+        email_body = _('Hi yshalsager, I came across your website and would like to get in touch.')
+        return render_template('contact.html', lang="ar", email_body=email_body,
+                               heading=_("Contact Me"), image=url_for('static', filename='img/contact.webp'))
--- personal_website/main/en/views.py
+++ personal_website/main/en/views.py
@@ -12,30 +12,31 @@ def index_redirect():
 
 @main.route('/en/')
 def index_en():
-    return render_template('index.html', lang="en")
+    return render_template('index.html', lang="en", image=url_for('static', filename='img/home-bg.jpg'))
 
 
 @main.route('/en/about')
 def about_en():
-    return render_template('about.html',
-                           lang="en",
-                           heading=_("About Me"),
-                           image=url_for('static', filename='img/home-bg.jpg'))
+    return render_template('about.html', lang="en",
+                           heading=_("About Me"), image=url_for('static', filename='img/home-bg.jpg'))
 
 
 @main.route('/en/blog')
 def blog_en():
     posts = get_blog_posts('en')
-    return render_template('blog.html',
-                           heading=_("Blog"),
+    return render_template('blog.html', heading=_("Blog"),
                            image=url_for('static', filename='img/home-bg.jpg'),
-                           lang='en',
-                           posts=posts)
+                           lang='en', posts=posts)
 
 
 @main.route('/en/projects')
 def projects_en():
-    return render_template('projects.html',
-                           lang="en",
-                           heading=_("My Works and Projects"),
-                           image=url_for('static', filename='img/home-bg.jpg'))
+    return render_template('projects.html', lang="en",
+                           heading=_("My Works and Projects"), image=url_for('static', filename='img/home-bg.jpg'))
+
+
+@main.route('/en/contact')
+def contact_en():
+    email_body = _('Hi yshalsager, I came across your website and would like to get in touch.')
+    return render_template('contact.html', lang="en", email_body=email_body,
+                           heading=_("Contact Me"), image=url_for('static', filename='img/contact.webp'))
