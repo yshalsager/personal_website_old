e4f047aebdbc9fd16ee7ff6add70f3118e125ccc
yshalsager <ysh-alsager@hotmail.com>
2020-09-12 14:32:28 +0200

    config: Add BABEL_TRANSLATION_DIRECTORIES path dynamically
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- config.py
+++ config.py
@@ -1,4 +1,8 @@
+from pathlib import Path
+
+
 class Config:
     LANGUAGES = ['en', 'ar']
     POSTS_DIR = 'posts'
     SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS = True
+    BABEL_TRANSLATION_DIRECTORIES = str(Path(__file__).absolute().parent / 'personal_website/translations')
