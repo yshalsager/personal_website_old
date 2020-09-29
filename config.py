b792170ffbd02f7d6d808ea5e1804c5456ab4088
yshalsager <ysh-alsager@hotmail.com>
2020-09-29 17:22:49 +0200

    Add advanced logging
    logger file is based on https://github.com/tenable/flask-logging-demo/blob/master/app_factory_pattern/log_demo_factory/flask_logs.py
    
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- config.py
+++ config.py
@@ -6,3 +6,12 @@ class Config:
     POSTS_DIR = 'posts'
     SITEMAP_INCLUDE_RULES_WITHOUT_PARAMS = True
     BABEL_TRANSLATION_DIRECTORIES = str(Path(__file__).absolute().parent / 'personal_website/translations')
+    # Logging
+    LOG_TYPE = "watched"
+    LOG_LEVEL = "INFO"
+    # Logging file Setup
+    LOG_DIR = f"{str(Path(__file__).absolute().parent)}/logs/"
+    APP_LOG_NAME = "app.log"
+    WWW_LOG_NAME = "www.log"
+    LOG_MAX_BYTES = 100000000
+    LOG_COPIES = 5
--- personal_website/__init__.py
+++ personal_website/__init__.py
@@ -1,12 +1,17 @@
-from flask import Flask
+import logging
+from datetime import datetime
+
+from flask import Flask, request
 from flask_babel import Babel
 from flask_sitemap import Sitemap
 
 from config import Config
+from logger import LogSetup
 from .main.posts import add_posts_routes
 
 babel = Babel()
 sitemap = Sitemap()
+logs = LogSetup()
 
 
 def create_app():
@@ -14,10 +19,30 @@ def create_app():
     app.config.from_object(Config)
     babel.init_app(app)
     sitemap.init_app(app)
+    logs.init_app(app)
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
     add_posts_routes(app)
+
+    @app.after_request
+    def after_request(response):
+        """ Logging after every request. """
+        logger = logging.getLogger("app.access")
+        logger.info(
+            "%s [%s] %s %s %s %s %s %s %s",
+            request.remote_addr,
+            datetime.utcnow().strftime("%d/%b/%Y:%H:%M:%S.%f")[:-3],
+            request.method,
+            request.path,
+            request.scheme,
+            response.status,
+            response.content_length,
+            request.referrer,
+            request.user_agent,
+        )
+        return response
+
     return app
 
 # @babel.localeselector
